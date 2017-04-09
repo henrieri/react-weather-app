5e562f97eb8e9e600fc781b72a4d00c0452f50d6
Henri Erilaid <henri.erilaid@gmail.com>
2017-04-09 23:33:36 +0300

    Remove unnecessary functions

--- src/App.js
+++ src/App.js
@@ -7,47 +7,6 @@ import './App.css';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      renderModule: this.getSearchView(),
-      geoLocationAvailable: this.geoLocationAvailable(),
-      results: {}
-    };
-  }
-
-  geoLocationAvailable() {
-    return ("geolocation" in navigator);
-  }
-
-  search() {
-    this.setState({
-      renderModule: this.getResultsView()
-    });
-  }
-
-  goBackToSearch() {
-    this.setState({
-      renderModule: this.getSearchView()
-    });
-  }
-
-  goToResults(results) {
-    this.setState({
-      results
-    });
-
-    this.search();
-  }
-
-  getSearchView() {
-    return (<Search goToResults={this.goToResults.bind(this)} />);
-  }
-
-  getResultsView() {
-    return (<Results results={this.state.results} goBackToSearch={this.goBackToSearch.bind(this)} />)
-  }
-
   render() {
     return (<Router>
       <div className="app--container">
